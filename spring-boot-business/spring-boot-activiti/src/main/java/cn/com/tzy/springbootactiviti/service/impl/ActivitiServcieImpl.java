5ed44750f7d78c66edaed3e23ee8c4ea33f6fee5
18789432816@126.com <18789432816@126.com>
2023-09-14 00:45:39 +0800

    修复 activiti 流程图 乱码 以及流程图乱序

--- spring-boot-business/spring-boot-activiti/src/main/java/cn/com/tzy/springbootactiviti/service/impl/ActivitiServcieImpl.java
+++ spring-boot-business/spring-boot-activiti/src/main/java/cn/com/tzy/springbootactiviti/service/impl/ActivitiServcieImpl.java
@@ -330,9 +330,9 @@ public class ActivitiServcieImpl extends BaseWorkflowService implements Activiti
             List<HistoricActivityInstance> historicActivityInstanceList = getProcessEngine().getHistoryService().createHistoricActivityInstanceQuery()
                     .processInstanceId(instanceId)
                     .activityTenantId(TenantContextHolder.getTenantId().toString())
-                    .orderByHistoricActivityInstanceEndTime().asc()
+                    .orderByHistoricActivityInstanceStartTime().asc()
                     .list();
-            List<NotNullMap> maps = historicActivityInstanceList.stream().map(activityInstance -> {
+            List<NotNullMap> maps = historicActivityInstanceList.stream().sorted(Comparator.comparing(HistoricActivityInstance::getStartTime)).map(activityInstance -> {
                 return new NotNullMap() {{
                     putString("activityName", activityInstance.getActivityName());
                     putString("processInstanceId", activityInstance.getProcessInstanceId());
