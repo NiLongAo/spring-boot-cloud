a4189b9d6055156dff9b21dbb5392e6a0ab600f1
TZY <18789432816@126.com>
2024-03-07 00:19:22 +0800

    部分优化提交

--- spring-boot-starter/spring-boot-starter-mybatis/src/main/java/cn/com/tzy/spingbootstartermybatis/config/MybatisPlusConfig.java
+++ spring-boot-starter/spring-boot-starter-mybatis/src/main/java/cn/com/tzy/spingbootstartermybatis/config/MybatisPlusConfig.java
@@ -1,11 +1,18 @@
 package cn.com.tzy.spingbootstartermybatis.config;
 
 import cn.com.tzy.spingbootstartermybatis.core.mapper.handler.DefaultDBFieldHandler;
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 public class MybatisPlusConfig {
@@ -13,12 +20,34 @@ public class MybatisPlusConfig {
     public MetaObjectHandler defaultMetaObjectHandler(){
         return new DefaultDBFieldHandler(); // 自动填充参数类
     }
-
+    /**
+     * 动态数据源配置项
+     */
+    @Resource
+    private DynamicDataSourceProperties dynamicDataSourceProperties;
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
         return mybatisPlusInterceptor;
+    }
+
+    /**
+     * 将动态数据源设置为首选的 （此处可以不注入 此对象 因为配置里已经表明了primary数据源）
+     * 当spring存在多个数据源时, 自动注入的是首选的对象
+     * 设置为主要的数据源之后，就可以支持shardingjdbc原生的配置方式了
+     */
+    @Primary
+    @Bean
+    public DataSource dataSource(DynamicDataSourceProvider dynamicDataSourceProvider) {
+        DynamicRoutingDataSource dataSource = new DynamicRoutingDataSource();
+        dataSource.setPrimary(dynamicDataSourceProperties.getPrimary());
+        dataSource.setStrict(dynamicDataSourceProperties.getStrict());
+        dataSource.setStrategy(dynamicDataSourceProperties.getStrategy());
+        dataSource.setProvider(dynamicDataSourceProvider);
+        dataSource.setP6spy(dynamicDataSourceProperties.getP6spy());
+        dataSource.setSeata(dynamicDataSourceProperties.getSeata());
+        return dataSource;
     }
 }
--- spring-boot-starter/spring-boot-starter-mybatis/src/main/java/cn/com/tzy/spingbootstartermybatis/config/ShardingDataSourceConfig.java
+++ spring-boot-starter/spring-boot-starter-mybatis/src/main/java/cn/com/tzy/spingbootstartermybatis/config/ShardingDataSourceConfig.java
@@ -68,21 +68,5 @@ public class ShardingDataSourceConfig {
         };
     }
 
-    /**
-     * 将动态数据源设置为首选的 （此处可以不注入 此对象 因为配置里已经表明了primary数据源）
-     * 当spring存在多个数据源时, 自动注入的是首选的对象
-     * 设置为主要的数据源之后，就可以支持shardingjdbc原生的配置方式了
-     */
-    @Primary
-    @Bean
-    public DataSource dataSource(DynamicDataSourceProvider dynamicDataSourceProvider) {
-        DynamicRoutingDataSource dataSource = new DynamicRoutingDataSource();
-        dataSource.setPrimary(dynamicDataSourceProperties.getPrimary());
-        dataSource.setStrict(dynamicDataSourceProperties.getStrict());
-        dataSource.setStrategy(dynamicDataSourceProperties.getStrategy());
-        dataSource.setProvider(dynamicDataSourceProvider);
-        dataSource.setP6spy(dynamicDataSourceProperties.getP6spy());
-        dataSource.setSeata(dynamicDataSourceProperties.getSeata());
-        return dataSource;
-    }
+
 }
