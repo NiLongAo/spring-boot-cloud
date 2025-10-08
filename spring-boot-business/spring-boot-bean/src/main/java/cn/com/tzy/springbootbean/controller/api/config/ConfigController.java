89c8c487386eee0db7c481e245cec9baa70899fb
tzy <18789432816@126.com>
2025-10-08 21:50:20 +0800

    部分优化提交

--- spring-boot-business/spring-boot-bean/src/main/java/cn/com/tzy/springbootbean/controller/api/config/ConfigController.java
+++ spring-boot-business/spring-boot-bean/src/main/java/cn/com/tzy/springbootbean/controller/api/config/ConfigController.java
@@ -7,8 +7,6 @@ import cn.com.tzy.springbootcomm.common.vo.RestResult;
 import cn.com.tzy.springbootentity.dome.sys.Config;
 import cn.com.tzy.springbootentity.param.sys.ConfigParam;
 import cn.com.tzy.springbootstartercloud.api.ApiController;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,41 +21,38 @@ public class ConfigController extends ApiController {
 
     @GetMapping("all")
     @ResponseBody
-    public RestResult<?> findAll(){
+    public RestResult<?> findAll() {
         return configService.findAll();
     }
 
     /**
      * 根据用户账号获取用户信息
+     * 
      * @return
      */
     @PostMapping("page")
     @ResponseBody
-    public PageResult page(@Validated @RequestBody ConfigParam userPageModel){
+    public PageResult page(@Validated @RequestBody ConfigParam userPageModel) {
         return configService.page(userPageModel);
     }
 
-
-
     @PostMapping("update")
     @ResponseBody()
-    public RestResult<?> update(@RequestBody @Validated ConfigParam params){
+    public RestResult<?> update(@RequestBody @Validated ConfigParam params) {
         Config config = configService.find(params.k);
-        if(config == null){
-            return RestResult.result(RespCode.CODE_2.getValue(),"未获取到系统配置信息");
+        if (config == null) {
+            return RestResult.result(RespCode.CODE_2.getValue(), "未获取到系统配置信息");
         }
         config.setConfigName(params.configName);
         config.setV(params.v);
         configService.update(config);
-        return RestResult.result(RespCode.CODE_0.getValue(),"修改成功");
+        return RestResult.result(RespCode.CODE_0.getValue(), "修改成功");
     }
 
     @GetMapping("detail")
     @ResponseBody
-    public RestResult<?> detail(@RequestParam("k") String k){
-        return  RestResult.result(RespCode.CODE_0.getValue(),null,configService.find(k));
+    public RestResult<?> detail(@RequestParam("k") String k) {
+        return RestResult.result(RespCode.CODE_0.getValue(), null, configService.find(k));
     }
-
-
 
 }
--- spring-boot-business/spring-boot-bean/src/main/java/cn/com/tzy/springbootbean/service/api/impl/ConfigServiceImpl.java
+++ spring-boot-business/spring-boot-bean/src/main/java/cn/com/tzy/springbootbean/service/api/impl/ConfigServiceImpl.java
@@ -2,7 +2,6 @@ package cn.com.tzy.springbootbean.service.api.impl;
 
 import cn.com.tzy.springbootbean.mapper.sql.ConfigMapper;
 import cn.com.tzy.springbootbean.service.api.ConfigService;
-import cn.com.tzy.springbootcomm.common.enumcom.ConstEnum;
 import cn.com.tzy.springbootcomm.constant.NotNullMap;
 import cn.com.tzy.springbootcomm.common.vo.PageResult;
 import cn.com.tzy.springbootcomm.common.vo.RespCode;
@@ -11,13 +10,12 @@ import cn.com.tzy.springbootentity.dome.sys.Config;
 import cn.com.tzy.springbootentity.param.sys.ConfigParam;
 import cn.com.tzy.springbootstarterredis.common.RedisCommon;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,9 +23,8 @@ import java.util.List;
 @Service
 public class ConfigServiceImpl extends ServiceImpl<ConfigMapper, Config> implements ConfigService {
 
-
     @Override
-    @Cacheable(value = RedisCommon.CONFIG_INFO,key = " #k")
+    @Cacheable(value = RedisCommon.CONFIG_INFO, key = " #k")
     public Config find(String k) {
         return baseMapper.selectOne(new LambdaQueryWrapper<Config>().eq(Config::getK, k));
     }
@@ -51,22 +48,22 @@ public class ConfigServiceImpl extends ServiceImpl<ConfigMapper, Config> impleme
     @Cacheable(value = RedisCommon.CONFIG_ALL_INFO)
     public RestResult<?> findAll() {
         List<NotNullMap> data = new ArrayList<>();
-        List<Config> configList = baseMapper.selectList(new LambdaQueryWrapper<Config>().orderByDesc(Config::getConfigName, Config::getK));
-        configList.forEach(obj-> {
+        List<Config> configList = baseMapper
+                .selectList(Wrappers.<Config>lambdaQuery().orderByDesc(Config::getConfigName, Config::getK));
+        configList.forEach(obj -> {
             NotNullMap map = new NotNullMap();
-            map.putString("k",obj.getK());
-            map.putString("v",obj.getV());
-            map.putString("configName",obj.getConfigName());
+            map.putString("k", obj.getK());
+            map.putString("v", obj.getV());
+            map.putString("configName", obj.getConfigName());
             data.add(map);
         });
-        return RestResult.result(RespCode.CODE_0.getValue(),null,data);
+        return RestResult.result(RespCode.CODE_0.getValue(), null, data);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    @CacheEvict(value = {RedisCommon.CONFIG_INFO,RedisCommon.CONFIG_ALL_INFO},allEntries = true)
+    @CacheEvict(value = { RedisCommon.CONFIG_INFO, RedisCommon.CONFIG_ALL_INFO }, allEntries = true)
     public int update(Config config) {
-        return baseMapper.update(config,new LambdaQueryWrapper<Config>().eq(Config::getK, config.getK()));
+        return baseMapper.update(config, new LambdaQueryWrapper<Config>().eq(Config::getK, config.getK()));
     }
 }
-
